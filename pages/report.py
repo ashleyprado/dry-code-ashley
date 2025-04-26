7a0b1551186019ce1e23172a9c522bd102858835
Ashley Prado <139910977+ashleyprado@users.noreply.github.com>
2025-04-26 01:00:45 -0400

    Update report.py
    refactored code

--- pages/report.py
+++ pages/report.py
@@ -1,34 +1,20 @@
 import streamlit as st
-
-if st.session_state.get("logged_in") == None:
-    st.session_state["logged_in"] = False
-
-
-def login():
-    st.session_state.logged_in = True
-
-
-def logout():
-    st.session_state.logged_in = False
-
+from common import initialize_session, login_sidebar, show_footer
 
 st.set_page_config(page_title="Report")
+
+initialize_session()
 
 st.markdown("# Report")
 st.sidebar.header("Report")
 
-if st.session_state.logged_in:
-    st.sidebar.success("Logged in")
-    st.sidebar.button("Log out", key="logout", on_click=logout)
-else:
-    st.sidebar.warning("Not logged in")
-    st.sidebar.button("Log in", key="login", on_click=login)
+login_sidebar()
 
-st.sidebar.write("This site is copyright Fake Company LLC Inc., 2024")
+show_footer()
 
 st.write(
     """
-        Here is a page with a report on it.
+    Here is a page with a report on it.
     """
 )
 
@@ -37,14 +23,14 @@ st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
 st.write(
     """
     Look at those numbers. Amazing.
-"""
+    """
 )
 
 with st.expander("Company Info"):
     st.write(
         """
         Fake Company LLC Inc. is located at 1600 Amphitheatre Parkway Mountain View, CA 94043
-    """
+        """
     )
 
 with st.expander("Links"):
@@ -55,5 +41,5 @@ with st.expander("Links"):
         [Gemini](https://gemini.google.com)
 
         [Streamlit Docs](https://docs.streamlit.io/)
-    """
+        """
     )
