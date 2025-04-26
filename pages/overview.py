e1485c443864fd206e8961048806cf2bde3f36d3
Ashley Prado <139910977+ashleyprado@users.noreply.github.com>
2025-04-26 01:00:02 -0400

    Update overview.py
    refactored code

--- pages/overview.py
+++ pages/overview.py
@@ -1,30 +1,16 @@
 import streamlit as st
-
-if st.session_state.get("logged_in") == None:
-    st.session_state["logged_in"] = False
-
-
-def login():
-    st.session_state.logged_in = True
-
-
-def logout():
-    st.session_state.logged_in = False
-
+from common import initialize_session, login_sidebar, show_footer
 
 st.set_page_config(page_title="Overview")
+
+initialize_session()
 
 st.markdown("# Overview")
 st.sidebar.header("Overview")
 
-if st.session_state.logged_in:
-    st.sidebar.success("Logged in")
-    st.sidebar.button("Log out", on_click=logout)
-else:
-    st.sidebar.warning("Not logged in")
-    st.sidebar.button("Log in", on_click=login)
+login_sidebar()
 
-st.sidebar.write("This site is copyright Fake Company LLC Inc., 2024")
+show_footer()
 
 st.write(
     """Here is a page with a site overview.
@@ -35,11 +21,13 @@ st.write(
     """
 )
 
+st.image("./assets/fake_company.png")  # (Optional)
+
 with st.expander("Company Info"):
     st.write(
         """
         Fake Company LLC Inc. is located at 1600 Amphitheatre Parkway Mountain View, CA 94043
-    """
+        """
     )
 
 with st.expander("Links"):
@@ -50,5 +38,5 @@ with st.expander("Links"):
         [Gemini](https://gemini.google.com)
 
         [Streamlit Docs](https://docs.streamlit.io/)
-    """
+        """
     )
